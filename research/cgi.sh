a5dcc016370d910107945dd7e71d43f3098c4d5d
Paul OHL <pohl@student.42.fr>
2022-03-22 14:13:58 +0100

    Added a few research files

---
+++ research/cgi.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# export urlReceived="/cgi-bin/ubuntu_cgi_tester/mnt/nfs/homes/pohl/Documents/tmp"
+
+export REQUEST_METHOD="POST"
+export SERVER_PROTOCOL="HTTP/1.1"
+export PATH_INFO="/"
+
+./ubuntu_cgi_tester
