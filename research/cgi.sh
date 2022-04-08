4c0b2ae7463dbc658752e8ebf171993c895a40ff
Paul OHL <paul.lv.ohl@gmail.com>
2022-04-08 11:49:37 +0200

    Removed useless directory research

--- research/cgi.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# export urlReceived="/cgi-bin/ubuntu_cgi_tester/mnt/nfs/homes/pohl/Documents/tmp"
-
-export REQUEST_METHOD="POST"
-export SERVER_PROTOCOL="HTTP/1.1"
-export PATH_INFO="/"
-
-./ubuntu_cgi_tester	./config.bnf
